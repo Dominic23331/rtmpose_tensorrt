af16ef8ffd97e937a111f9d8d30ae12623d185ce
yang_guo123 <1179039628@qq.com>
2023-05-24 11:05:29 +0800

    添加项目文件。

---
+++ rtmpose_tensorrt/inference.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#include <NvInfer.h>
+#include <opencv2/opencv.hpp>
+
+#include "rtmdet.h"
+#include "rtmpose.h"
+#include "utils.h"
+
+
+
+std::vector<std::vector<PosePoint>> inference(cv::Mat& image, RTMDet& detect_model, RTMPose& pose_model);
+
+
